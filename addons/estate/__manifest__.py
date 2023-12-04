f659e9f868ec64a4520d3ca170bb7ddd376f6828
Akecheta <wassimbensalah.eng@gmail.com>
2023-12-04 16:07:50 +0100

    Creating a Real Estate Advertisment Module (Chapter 6 & 7)

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -1,9 +1,12 @@
 {
 'name': "Estate",
+'author': "Wassim Ben Salah",
 'depends': ['base'],
 'license': 'LGPL-3',
 
 'data': [
 'security/ir.model.access.csv',
+'views/estate_property_views.xml',
+'data/estate_menus.xml'
 ]
 }
--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -3,18 +3,23 @@ from odoo import fields, models
 
 class Property(models.Model):
     _name = "estate_property"
-    _description = "Proprety of the estate"
+    _description = "Property of the estate"
 
     name = fields.Char('Title', required=True)
     description = fields.Text('Description')
     postcode = fields.Integer('Postcode')
-    data_availability = fields.Date('Data Availability')
+    data_availability = fields.Date('Data Availability', copy=False,
+                                    default=fields.Date.add(fields.Date.today(), month=3))
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
     living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
-    garden_orientation = fields.Selection([('north','North'), ('south','South'),('east','East'),('west','West')])
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    active = fields.Boolean('Active', default=True)
+    state = fields.Selection(
+        [('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'),
+         ('canceled', 'Canceled')], default='new', required=True, copy=False)
